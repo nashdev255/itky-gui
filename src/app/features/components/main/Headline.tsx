db81129da6114609a29ecdf58dbfb949cfca461d
nashdev255 <nashdev255@gmail.com>
2024-03-21 02:03:58 +0900

    Change font family

--- src/app/features/components/main/Headline.tsx
+++ src/app/features/components/main/Headline.tsx
@@ -7,7 +7,7 @@ const Headline = () => {
     <>
       <div className="mb-8 flex">
         <div className='flex-col space-y-4'>
-          <h1 className='font-sanserif text-4xl font-bold drop-shadow-lg md:text-5xl'>Welcome, Developpers!</h1>
+          <h1 className='font-sans text-4xl font-bold drop-shadow-lg md:text-5xl'>Welcome, Developpers!</h1>
           <div className='flex justify-center'>
             <Link href={'/dashboard/monsters'} className='rounded-full bg-black px-4 py-2 text-white shadow-lg hover:scale-110'>
               <p className='font-bold'>編集する</p>
