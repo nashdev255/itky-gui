97fad50a5a824b9dc684cafb155ee10eeba539dc
nashdev255 <nashdev255@gmail.com>
2024-03-21 00:13:32 +0900

    Add main

--- src/app/features/components/main/Main.tsx
+++ src/app/features/components/main/Main.tsx
@@ -1,6 +1,12 @@
+'use client';
+
+import HeadLine from '@/app/features/components/main/Headline';
+
 const Main = () => {
   return (
-    <></>
+    <main className="flex flex-1 items-center justify-center bg-[url('/Itokiyostatue.png')] bg-center bg-no-repeat px-[8vw] py-4">
+      <HeadLine />
+    </main>
   );
 };
 
