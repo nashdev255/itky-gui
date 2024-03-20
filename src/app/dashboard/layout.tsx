063310cfefa3bdb693e950d56c068798f6304d2c
nashdev255 <nashdev255@gmail.com>
2024-03-21 01:35:49 +0900

    Update DashboardLayout

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,6 +1,11 @@
+import FloatingButton from '@/app/features/components/dashboard/FloatingButton';
+
 const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
   return (
-    <></>
+    <>
+      { children }
+      <FloatingButton />
+    </>
   );
 };
 
