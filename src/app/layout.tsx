07313b14e7e7e80a013ec9068aba5eece5ccc221
nashdev255 <nashdev255@gmail.com>
2024-03-21 00:19:56 +0900

    Update RootLayout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,12 @@
-import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import './globals.css';
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
-  title: "イトクエ制作委員会公式サイト",
-  description: "イトキヨクエスト制作委員会公式サイトです。",
+  title: 'イトクエ制作委員会公式サイト',
+  description: 'イトキヨクエスト制作委員会公式サイトです。',
 };
 
 export default function RootLayout({
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} flex min-h-screen flex-col`}>{children}</body>
     </html>
   );
 }
