2bb82e1a54834edf3a64e5d068f56356bbeb2fdc
nashdev255 <nashdev255@gmail.com>
2024-03-21 01:49:31 +0900

    impl BasicAuth

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,8 +1,31 @@
-import { NextResponse } from 'next/server';
+'use server';
+
 import type { NextRequest } from 'next/server';
- 
-export function middleware(request: NextRequest) {
-  if (request.nextUrl.pathname.endsWith('/dashboard')) {
-    return NextResponse.redirect(new URL('/dashboard/monsters', request.url));
+import { NextResponse } from 'next/server';
+
+const basicAuthUser = process.env.NEXT_PUBLIC_BASIC_AUTH_USER!;
+const basicAuthPassword = process.env.NEXT_PUBLIC_BASIC_AUTH_PASSWORD!;
+
+function middleware(req: NextRequest): NextResponse {
+  const basicAuth = req.headers.get('authorization');
+  const url = req.nextUrl;
+
+  if ( basicAuth ) {
+    const authValue = basicAuth.split(' ')[1] ?? '';
+    const [user, pwd] = atob(authValue).split(':');
+
+    if ( user === basicAuthUser && pwd === basicAuthPassword ) {
+      return NextResponse.next();
+    }
   }
+
+  url.pathname = '/api/basic-auth';
+
+  return NextResponse.rewrite(url);
 }
+
+export { middleware };
+
+// if (req.nextUrl.pathname.endsWith('/dashboard')) {
+//   return NextResponse.redirect(new URL('/dashboard/monsters', req.url));
+// }
