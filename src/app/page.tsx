818e3b77e850d2f5063b94c351146f6115e0504e
nashdev255 <nashdev255@gmail.com>
2024-03-21 00:07:38 +0900

    Add Home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
+import Header from '@/app/features/components/header/Header';
+import Main from '@/app/features/components/main/Main';
+import Footer from '@/app/features/components/footer/Footer';
+
 export default function Home() {
   return (
-    <></>
+    <>
+      <Header />
+      <Main />
+      <Footer />
+    </>
   );
 }
