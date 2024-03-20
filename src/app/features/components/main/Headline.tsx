9264860ddb627b1c741d94a2307120b499ce3219
nashdev255 <nashdev255@gmail.com>
2024-03-21 01:36:38 +0900

    Change Link href

--- src/app/features/components/main/Headline.tsx
+++ src/app/features/components/main/Headline.tsx
@@ -9,7 +9,7 @@ const Headline = () => {
         <div className='flex-col space-y-4'>
           <h1 className='font-sanserif text-4xl font-bold drop-shadow-lg md:text-5xl'>Welcome, Developpers!</h1>
           <div className='flex justify-center'>
-            <Link href={'/dashboard/edit'} className='rounded-full bg-black px-4 py-2 text-white shadow-lg hover:scale-110'>
+            <Link href={'/dashboard/'} className='rounded-full bg-black px-4 py-2 text-white shadow-lg hover:scale-110'>
               <p className='font-bold'>編集する</p>
             </Link>
           </div>
