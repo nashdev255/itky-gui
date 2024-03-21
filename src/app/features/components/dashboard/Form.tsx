1434d261f2e62ad15ef0954b2769c23fb68ff8c7
nashdev255 <nashdev255@gmail.com>
2024-03-22 08:03:56 +0900

    Change the button appearance a bit

--- src/app/features/components/dashboard/Form.tsx
+++ src/app/features/components/dashboard/Form.tsx
@@ -22,7 +22,7 @@ const Form = () => {
           <option>Materials</option>
         </select>
       </div>
-      <button className='absolute bottom-8 right-8 h-14 w-28 rounded-full bg-sky-400 shadow-lg hover:scale-95 hover:bg-sky-500 hover:shadow-none'>
+      <button className='absolute bottom-8 right-8 h-14 w-28 rounded-full border-2 border-sky-500 bg-sky-400 shadow-lg hover:scale-95 hover:bg-sky-500 hover:shadow-none'>
         <p className='text-lg font-bold text-white'>送信</p>
       </button>
     </form>
