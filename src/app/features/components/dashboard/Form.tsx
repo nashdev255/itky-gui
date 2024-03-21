9416a9b53288607b26225ff8900cfdcbfb9e9930
nashdev255 <nashdev255@gmail.com>
2024-03-22 08:49:22 +0900

    Add Loading

--- src/app/features/components/dashboard/Form.tsx
+++ src/app/features/components/dashboard/Form.tsx
@@ -1,6 +1,46 @@
-const Form = () => {
-  const handleSubmit = () => {
+'use client';
 
+import { useState } from 'react';
+import Loading from '@/app/components/Loading';
+import SubmitButton from '@/app/features/components/dashboard/SubmitButton';
+
+const Form = () => {
+  const [loading, setLoading] = useState(false);
+  const [message, setMessage] = useState<string | null>(null);
+
+  const handleSubmit = async () => {
+    setLoading(true);
+
+    const formElement = document.querySelector('form');
+    const formData = new FormData(formElement!);
+
+    const editTarget = (() => {
+      const et = formData.get('editTarget')?.toString();
+      if ( et === 'Monsters' )  return 'monster';
+      if ( et === 'Weapons' )   return 'weapon';
+      if ( et === 'Armors' )    return 'armor';
+      if ( et === 'Items' )     return 'item';
+      if ( et === 'Materials' ) return 'material';
+    })();
+
+    try {
+      await fetch(
+        'https://api.github.com/repos/nashdev255/github-dispatch-test/actions/workflows/echo.yml/dispatches',
+        {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/vnd.github+json',
+            'Authorization': `Bearer ${process.env.NEXT_PUBLIC_GITHUB_TOKEN}`,
+          },
+          body: `{"ref":"master","inputs":{"message": "${editTarget}"}}`
+        }
+      );
+    } catch ( error ) {
+      setMessage('' + error);
+      alert('' + error);
+    } finally {
+      setLoading(false);
+    }
   };
 
   return (
@@ -12,7 +52,7 @@ const Form = () => {
       <div className='mb-8 flex space-x-4'>
         <p className='flex items-center text-xl'>追加項目</p>
         <select
-          name=""
+          name="editTarget"
           className='border-2 px-3 py-2'
         >
           <option>Monsters</option>
@@ -22,9 +62,15 @@ const Form = () => {
           <option>Materials</option>
         </select>
       </div>
-      <button className='absolute bottom-8 right-8 h-14 w-28 rounded-full border-2 border-sky-500 bg-sky-400 shadow-lg hover:scale-95 hover:bg-sky-500 hover:shadow-none'>
-        <p className='text-lg font-bold text-white'>送信</p>
-      </button>
+      <div className='absolute bottom-8 right-8'>
+        {loading ? (
+          <div className='flex h-14 w-28 items-center justify-center'>
+            <Loading />
+          </div>
+        ) : (
+          <SubmitButton />
+        )}
+      </div>
     </form>
   );
 };
---
+++ src/app/features/components/dashboard/SubmitButton.tsx
@@ -0,0 +1,9 @@
+const SubmitButton = () => {
+  return (
+    <button className='h-14 w-28 rounded-full border-2 border-sky-500 bg-sky-400 shadow-lg hover:scale-95 hover:bg-sky-500 hover:shadow-none'>
+      <p className='text-lg font-bold text-white'>送信</p>
+    </button>
+  );
+};
+
+export default SubmitButton;
