e76a76c841adfdd37ab0a048f3427ce0a595bdc2
nashdev255 <nashdev255@gmail.com>
2024-03-22 08:02:21 +0900

    Add form submit button

---
+++ src/app/features/components/dashboard/Form.tsx
@@ -0,0 +1,32 @@
+const Form = () => {
+  const handleSubmit = () => {
+
+  };
+
+  return (
+    <form
+      onSubmit={handleSubmit}
+      className='p-8'
+    >
+      <h1 className='mb-8 px-8 text-2xl font-bold'>ドキュメントに追加する</h1>
+      <div className='mb-8 flex space-x-4'>
+        <p className='flex items-center text-xl'>追加項目</p>
+        <select
+          name=""
+          className='border-2 px-3 py-2'
+        >
+          <option>Monsters</option>
+          <option>Weapons</option>
+          <option>Armors</option>
+          <option>Items</option>
+          <option>Materials</option>
+        </select>
+      </div>
+      <button className='absolute bottom-8 right-8 h-14 w-28 rounded-full bg-sky-400 shadow-lg hover:scale-95 hover:bg-sky-500 hover:shadow-none'>
+        <p className='text-lg font-bold text-white'>送信</p>
+      </button>
+    </form>
+  );
+};
+
+export default Form;
--- src/app/features/components/dashboard/ModalForm.tsx
+++ src/app/features/components/dashboard/ModalForm.tsx
@@ -1,10 +1,12 @@
 'use client';
 
+import Form from '@/app/features/components/dashboard/Form';
 import { MdOutlineClose } from 'react-icons/md';
 
 const ModalForm = ({ handleClick }: { handleClick: Function }) => {
   return (
     <div className='fixed inset-0 z-50 m-auto h-[90vh] w-[80vw] rounded-xl border-2 bg-white'>
+      <Form />
       <button
         onClick={() => handleClick()}
         className='absolute right-4 top-4 rounded-lg p-2 duration-150 hover:bg-slate-600 hover:bg-opacity-[0.30]'
